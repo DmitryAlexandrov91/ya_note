210976ab6bad4074ae43675444ca8bd8bb7520c3
Dmitry Alexandrov <alexandrov.dm@mail.ru>
2024-08-12 21:38:47 +0300

    TestNoteCreation add

--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
 
-from notes.forms import NoteForm, WARNING
+from notes.forms import WARNING
 from notes.models import Note
 
 User = get_user_model()
@@ -25,47 +25,35 @@ class TestNoteCreation(TestCase):
             'title': cls.NOTE_TITLE,
             'text': cls.NOTE_TEXT,
             'slug': cls.NOTE_SLUG}
-        cls.note = Note.objects.create(
-            title=cls.NOTE_TITLE,
-            text=cls.NOTE_TEXT,
-            slug=cls.NOTE_SLUG,
-            author_id=cls.user.id
-        )
-        cls.add_url = reverse('notes:add')
-        cls.detail_url = reverse('notes:detail', args=(cls.note.slug,))
-        cls.edit_url = reverse('notes:edit', args=(cls.note.slug,))
-        cls.delete_url = reverse('notes:delete', args=(cls.note.slug,))
+        cls.url = reverse('notes:add')
 
     def test_anonymous_user_cant_create_note(self):
         """Анонимный юзер не может создать заметку."""
 
-        self.client.post(self.add_url, data=self.form_data)
+        self.client.post(self.url, data=self.form_data)
         notes_count = Note.objects.count()
-        self.assertEqual(notes_count, 1)
+        self.assertEqual(notes_count, 0)
 
     def test_auth_user_can_create_note(self):
         """Авторизованный юзер может создать заметку."""
 
-        self.auth_client.post(self.add_url, data=self.form_data)
+        self.auth_client.post(self.url, data=self.form_data)
         notes_count = Note.objects.count()
         self.assertEqual(notes_count, 1)
-
-    # def test_auth_user_can_edit_notes(self):
-    #     """Автор может редактировать заметку."""
-    #     # Выполняем запрос на редактирование от имени автора комментария.
-    #     response = self.author_client.post(self.edit_url, data=self.form_data)
-    #     # Проверяем, что сработал редирект.
-    #     self.assertRedirects(response, self.url_to_comments)
-    #     # Обновляем объект комментария.
-    #     self.comment.refresh_from_db()
-    #     # Проверяем, что текст комментария соответствует обновленному.
-    #     self.assertEqual(self.comment.text, self.NEW_COMMENT_TEXT)
 
     def test_not_unique_slug(self):
         """Тест на уникальность slug"""
 
-        not_unique_slug = {'slug': self.note.slug}
-        response = self.auth_client.post(self.add_url, data=not_unique_slug)
+        note = Note.objects.create(
+            title=self.NOTE_TITLE,
+            text=self.NOTE_TEXT,
+            slug=self.NOTE_SLUG,
+            author_id=self.user.id
+        )
+        not_unique_slug = {'slug': note.slug}
+        response = self.auth_client.post(
+            self.url,
+            data=not_unique_slug)
         self.assertFormError(
             response,
             form='form',
@@ -74,3 +62,7 @@ class TestNoteCreation(TestCase):
         )
         note_count = Note.objects.count()
         self.assertEqual(note_count, 1)
+
+
+# class TestNoteEditeDelete(TestCase):
+
